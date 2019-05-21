b7f4ff9ca02c262bf8c86acbe2b781dc4b75b984
priyanka <priyanka.kite@gmail.com>
2019-05-21 13:57:47 -0500

    modified file path to ACCESS images

--- maps.js
+++ maps.js
@@ -8,7 +8,7 @@ function showImage() {
   else {
     x.style.display = "none";
   }
-  var str = event.target.parentElement.childNodes[1].innerHTML.replace(/\s/g, "").concat(".jpg");
+  var str = event.target.parentElement.childNodes[2].innerHTML.replace(/\s/g, "").concat(".jpg");
   var imgSource = document.getElementById("outputImage");
   imgSource.setAttribute("width", 1000, "margin-left", 1000, "border-width", 100);
   imgSource.setAttribute("src", "img/Maps/" + str);
