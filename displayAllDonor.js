fe728a10352dacbfff38ca1a57315787ead542e7
priyanka <priyanka.kite@gmail.com>
2019-06-05 13:22:32 -0500

    avoided duplicate results

--- displayAllDonor.js
+++ displayAllDonor.js
@@ -12,20 +12,22 @@ function displayAllDonors() {
     var td2 = document.createElement('th');
     var td3 = document.createElement('th');
     var td4 = document.createElement('th');
+    var td5 = document.createElement('th');
     var text1 = document.createTextNode('Donor Name');
     var text2 = document.createTextNode('Personalization');
     var text3 = document.createTextNode('Zone');
+    var text4 = document.createTextNode('view');
     td2.appendChild(text1);
     td3.appendChild(text2);
     td4.appendChild(text3);
+    td5.appendChild(text4);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
+    tr.appendChild(td5);
     table.appendChild(tr);
   
     for (var x in bricksJSON) {
-      var toCheck = bricksJSON[x].DonorName.toLowerCase();
-      var descriptioncheck = bricksJSON[x].Personalization.toLowerCase();
       var text1 = document.createTextNode(bricksJSON[x].DonorName);
       var text2 = document.createTextNode(bricksJSON[x].Personalization);
       var text3 = document.createTextNode(bricksJSON[x].Zone);
@@ -33,8 +35,6 @@ function displayAllDonors() {
     }
   
     for (var x in paversJSON) {
-      var toCheck = paversJSON[x].DonorName.toLowerCase();
-      var descriptioncheck = paversJSON[x].Personalization.toLowerCase();
       var text1 = document.createTextNode(paversJSON[x].DonorName);
       var text2 = document.createTextNode(paversJSON[x].Personalization);
       var text3 = document.createTextNode(paversJSON[x].Zone);
@@ -42,39 +42,43 @@ function displayAllDonors() {
     }
   
   
-    for (var x in bricksJSON) {
-      var toCheck = bricksJSON[x].DonorName.toLowerCase();
-      var descriptioncheck = bricksJSON[x].Personalization.toLowerCase();
-        var word1 = "null";
-        word1 = bricksJSON[x].Personalization;
-        var text1 = document.createTextNode(bricksJSON[x].DonorName);
-        var text2 = document.createTextNode(bricksJSON[x].Personalization);
-        var text3 = document.createTextNode(bricksJSON[x].Zone); 
-        displayTable();
-    }
-    for (var x in paversJSON) {
-      var toCheck = paversJSON[x].DonorName.toLowerCase();
-      var descriptioncheck = paversJSON[x].Personalization.toLowerCase();
-        var word1 = "null";
-        word1 = paversJSON[x].Personalization;
-        var text1 = document.createTextNode(paversJSON[x].DonorName);
-        var text2 = document.createTextNode(paversJSON[x].Personalization);
-        var text3 = document.createTextNode(paversJSON[x].Zone);
-        displayTable();
+    // for (var x in bricksJSON) {
+    //   var toCheck = bricksJSON[x].DonorName.toLowerCase();
+    //   var descriptioncheck = bricksJSON[x].Personalization.toLowerCase();
+    //     var word1 = "null";
+    //     word1 = bricksJSON[x].Personalization;
+    //     var text1 = document.createTextNode(bricksJSON[x].DonorName);
+    //     var text2 = document.createTextNode(bricksJSON[x].Personalization);
+    //     var text3 = document.createTextNode(bricksJSON[x].Zone); 
+    //     displayTable();
+    // }
+    // for (var x in paversJSON) {
+    //   var toCheck = paversJSON[x].DonorName.toLowerCase();
+    //   var descriptioncheck = paversJSON[x].Personalization.toLowerCase();
+    //     var word1 = "null";
+    //     word1 = paversJSON[x].Personalization;
+    //     var text1 = document.createTextNode(paversJSON[x].DonorName);
+    //     var text2 = document.createTextNode(paversJSON[x].Personalization);
+    //     var text3 = document.createTextNode(paversJSON[x].Zone);
+    //     displayTable();
     
-    }
+    // }
     var x = document.createElement("H2");
     function displayTable() {
       var tr = document.createElement('tr');
       var td2 = document.createElement('td');
       var td3 = document.createElement('td');
       var td4 = document.createElement('td');
+      var td5 = document.createElement('td');
+      var text4 = document.createTextNode("👁");
       td2.appendChild(text1);
       td3.appendChild(text2);
       td4.appendChild(text3);
+      td5.appendChild(text4);
       tr.appendChild(td2);
       tr.appendChild(td3);
       tr.appendChild(td4);
+      tr.appendChild(td5);
       table.appendChild(tr);
       var node = document.createElement("p");
       node.appendChild(table);
